c9390e957e635c87b538dbcace5c92ccf25d04f9
Bennett <pbennett@19-MHF6VJH96P.local>
2026-06-23 10:58:56 +0100

    as per astral docs for ruff and conform
    https://docs.astral.sh/ruff/editors/setup/#neovim

--- .config/nvim/lua/plugins/default-tweaks/conform.lua
+++ .config/nvim/lua/plugins/default-tweaks/conform.lua
@@ -3,7 +3,14 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      python = { "ruff_organize_imports", "ruff_format" },
+      python = {
+        -- To fix auto-fixable lint errors.
+        "ruff_fix",
+        -- To run the Ruff formatter.
+        "ruff_format",
+        -- To organize the imports.
+        "ruff_organize_imports",
+      },
       sql = { "sqruff" },
       yaml = { "yamlfmt" }, -- Replace default with K8s-friendly formatter
     },
@@ -13,7 +20,9 @@ return {
         command = "sh",
         args = {
           "-c",
-          "sqruff fix --config " .. vim.fn.expand("~/.config/sqruff/.sqruff") .. " - | sed -e :a -e '/^$/{$d;N;ba' -e '}'",
+          "sqruff fix --config "
+            .. vim.fn.expand("~/.config/sqruff/.sqruff")
+            .. " - | sed -e :a -e '/^$/{$d;N;ba' -e '}'",
         },
         stdin = true,
       },
