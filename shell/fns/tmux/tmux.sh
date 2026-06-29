e71c4b5bc268be32929da43965a0ebb1d6225e03
Bennett <pbennett@19-MHF6VJH96P.local>
2026-06-29 21:03:51 +0100

    adding the sesh func back

--- shell/fns/tmux/tmux.sh
+++ shell/fns/tmux/tmux.sh
@@ -146,3 +146,22 @@ tsl() {
 
   tmux select-pane -t "${panes[0]}"
 }
+
+ts() {
+  local session
+  session=$(sesh list --icons | fzf-tmux -p 80%,70% \
+    --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
+    --header '  ^a all ^t tmux ^g configs ^x zoxide ^f find' \
+    --bind 'tab:down,btab:up' \
+    --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
+    --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+    --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
+    --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+    --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+    --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
+    --preview-window 'right:55%' \
+    --preview 'sesh preview {}' 2>/dev/null) ||
+    session=$(sesh list | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+  [[ -z "$session" ]] && return
+  sesh connect "$session"
+}
