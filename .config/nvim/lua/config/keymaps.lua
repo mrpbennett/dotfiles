6fd989d40190372945c4030a71bdcfb1cef1991c
Paul Bennett <pbennett.uk@outlook.com>
2026-07-10 12:00:59 +0100

    cleaning up tmux

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,11 +4,6 @@ local map = vim.keymap.set
 -- ============================================================================
 -- Normal Mode
 -- ============================================================================
--- Disable arrow keys in normal mode
-map("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-map("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-map("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-map("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- ============================================================================
 -- Insert Mode
