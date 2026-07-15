aea6ec58dfa11cb8a55ab48bd045ba8eecca57b6
Paul Bennett <pbennett.uk@outlook.com>
2026-07-15 17:51:02 +0100

    cleaning up envs
    And adding SUDO_EDITOR for Linux so I can use Lazyvim with sudo -e /
    sudoedit

--- .local/share/dotfiles/default/shell/envs.sh
+++ .local/share/dotfiles/default/shell/envs.sh
@@ -1,9 +1,11 @@
+export LANG=en_US.UTF-8
+export XDG_CONFIG_HOME="$HOME/.config"
+
+export EDITOR=nvim
+export SUDO_EDITOR=nvim
+
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml" # Starship Prompt
 
-# XDG BASE DIRECTORY ---
-export XDG_CONFIG_HOME="$HOME/.config"
-export LANG=en_US.UTF-8
-export EDITOR=/opt/homebrew/bin/nvim
 export POETRY_VIRTUALENVS_IN_PROJECT=true
 
 # opencode
