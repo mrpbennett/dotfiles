88b01c264d8c5cacdb0235c0484d2895634ebe42
Paul Bennett <pbennett.uk@outlook.com>
2026-07-31 16:13:52 +0100

    ..

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -9,7 +9,7 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader> ",
+      "<leader>y",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
