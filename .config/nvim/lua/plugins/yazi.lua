aed13fa39aef5699ee604a1ad80f722e1b05c361
Paul Bennett <pbennett.uk@outlook.com>
2026-07-31 15:03:12 +0100

    adding yazi flavours

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -9,7 +9,7 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>y",
+      "<leader> ",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
