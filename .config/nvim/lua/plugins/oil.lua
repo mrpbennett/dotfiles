915de7b5d46cf73f635af9d4c9cd7f38d7da19b2
Paul Bennett <pbennett.uk@outlook.com>
2026-09-02 14:40:07 +0100

    adding oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,11 +4,16 @@ return {
     ---@module 'oil'
     ---@type oil.SetupOpts
     opts = {
+      default_file_explorer = false,
       columns = {
         "icon",
-        "size",
       },
       delete_to_trash = true,
+      view_options = { show_hidden = true },
+      lsp_file_methods = {
+        autosave_changes = true,
+      },
+      watch_for_changes = true,
     },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
--- .config/nvim/lua/plugins/orbit.lua
+++ .config/nvim/lua/plugins/orbit.lua
@@ -1,7 +1,9 @@
 return {
   "mrpbennett/orbit.nvim",
   opts = {
-    saved_query_dir = "~/Developer/pulsepoint/pbennett-monorepo/sql",
+    saved_query_dirs = {
+      { work = "~/Developer/pulsepoint/pbennett-monorepo/sql" },
+    },
     schema_width = 40,
     winbar = true,
   },
