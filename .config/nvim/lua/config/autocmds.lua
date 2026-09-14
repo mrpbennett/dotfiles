8ae8036269b96a426bd3a1a8b003ad94c73c3e19
Paul Bennett <pbennett.uk@outlook.com>
2026-09-14 21:55:52 +0100

    herdr fix and trim whitespace on save in nvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -30,3 +30,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
     vim.opt.formatoptions:remove({ "r", "o" })
   end,
 })
+
+-- Trim whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+  end,
+})
